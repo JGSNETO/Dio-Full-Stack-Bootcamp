c87077560df064f0e6435b66fd2123774e42b4ae
José Neto <jgsnto.sd@gmail.com>
2022-10-01 11:08:34 -0300

    JS

---
+++ Javascript/helloworld.js
@@ -0,0 +1,14 @@
+// var y = 0;
+/*Comentário de várias linhas*/
+var x = 1, z = 2;
+var diadasemana = 'segunda';
+const PRECO = 2;
+
+function soma(a, b) {
+    console.log('Soma: ')
+    return a + b; 
+}
+
+soma(1, 2);
+
+
